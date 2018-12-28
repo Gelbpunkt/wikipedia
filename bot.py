6feee66c6be29fc189639bc3d0a67fc206063252
Gelbpunkt <jens@troet.org>
2018-12-28 15:30:21 +0000

    events added

--- bot.py
+++ bot.py
@@ -8,12 +8,14 @@ import config
 
 bot = commands.Bot(command_prefix="wiki ", case_insensitive=True)
 
+
 async def start():
     bot.session = aiohttp.ClientSession(loop=bot.loop)
     bot.wiki = aiowiki.Wiki.wikipedia("en", session=bot.session)
     for ext in config.exts:
         bot.load_extension(ext)
     await bot.start(config.token)
+
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(start())
---
+++ modules/events.py
@@ -0,0 +1,29 @@
+import discord
+import traceback
+from discord.ext import commands
+
+
+class Events:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+    async def on_command_error(self, ctx, error):
+        error = getattr(error, "original", error)
+        if isinstance(error, discord.Forbidden):
+            return
+        elif isinstance(error, commands.CommandNotFound):
+            return
+        elif isinstance(error, commands.NotOwner):
+            return await ctx.send("You're not the owner of this bot.")
+        print("Ignoring exception in command {}:".format(ctx.command), file=sys.stderr)
+        traceback.print_exception(
+            type(error), error, error.__traceback__, file=sys.stderr
+        )
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
