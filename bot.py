1f675ab400b8d66edbe36bfbf0c654514652ce04
Gelbpunkt <jens@troet.org>
2018-12-28 15:45:30 +0000

    sup f4 fix dis

--- bot.py
+++ bot.py
@@ -6,30 +6,27 @@ from discord.ext import commands
 
 import config
 
+
 class Wikipedia(commands.Bot):
-    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
         self.owners = [356091260429402122, 340745895932854272]
         self.session = None
-        
+
     def __repr__(self):
         return "Wikipedia Botto"
-    
+
     async def start(self):
         self.session = aiohttp.ClientSession(loop=self.loop)
-        
-        for ext in config.ext:
+
+        for ext in config.exts:
             self.load_extension(ext)
-        
+
         super().run(config.token, bot=True)
-        
+
+
 if __name__ == "__main__":
     Wikipedia(
-        command_prefix=commands.when_mentioned_or(
-            "wiki "
-        ),
-        case_insensitive=True
-    )
-    .run()
+        command_prefix=commands.when_mentioned_or("wiki "), case_insensitive=True
+    ).run()
